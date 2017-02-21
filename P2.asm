1481885c7c8b783a98ce809d6deddd7d9e7f3886
iRodri <iRodri@users.noreply.github.com>
2017-02-21 23:27:47 +0100

    Update P2.asm

--- P2.asm
+++ P2.asm
@@ -3,14 +3,16 @@
 ;  FECHA: 21 Febrero 2017
 ;  PRUEBAS REALIZADAS: Práctica 2 
 ; ---------------------------------
-.DATA 0071H
-  dB 1,2,3,4,5,0
-.ORG 100AH
-LXI H, 71H
-MVI B, 4
-REP: ADD M
-INX H
-DCR B
-JP REP
-MOV M, A
-HLT
+
+.DATA 0071H ; Compiler Directive
+  dB 1,2,3,4,5,0 ; Compiler Directive
+.ORG 100AH ; Compiler Directive
+
+LXI H, 71H ; Initialize H Pair 
+MVI B, 4 ; Initizalize B to (n-1)
+REP: ADD M ; Loop Marker: Increase A by M
+INX H ; Increase memory address by 1
+DCR B ; Decrease B by 1
+JP REP ; If B is positive go to REP
+MOV M, A ; Save to memory
+HLT ; End
