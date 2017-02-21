56c9dee2f99494310c2190663dd1d30ff8e02202
iRodri <iRodri@users.noreply.github.com>
2017-02-21 19:20:20 +0100

    Create P2.asm

---
+++ P2.asm
@@ -0,0 +1,8 @@
+; ---------------------------------
+;  AUTOR: Díaz García, Rodrigo
+;  FECHA: 21 Febrero 2017
+;  PRUEBAS REALIZADAS: Práctica 2 
+; ---------------------------------
+.DATA 0071H
+  dB 1,2,3,4,5
+.ORG 100AH
