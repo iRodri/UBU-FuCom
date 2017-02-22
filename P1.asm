52f243db76fc31c803aa01d3419838e4cd4099f0
rorik <about@rorik.me>
2017-02-22 17:59:06 +0100

    P1 1.1

--- P1.asm
+++ P1.asm
@@ -1,11 +1,12 @@
 ; ---------------------------------
-;  AUTOR: Díaz García, Rodrigo
-;  FECHA: 21 Febrero 2017
-;  PRUEBAS REALIZADAS: Práctica 1 
+;  AUTHOR: Díaz García, Rodrigo
+;  DATE: 21 February 2017
+;  ASSIGNMENT: P1
+;  REVISION: 1.1 - Updated comments
 ; ---------------------------------
 .ORG 100AH
 ; ------------
-;  Apartado 1
+;  Section 1
 ; ------------
 MVI A, 1H
 MVI B, 2H
@@ -15,11 +16,11 @@ MVI E, 5H
 MVI H, 6H
 MVI L, 7H
 ; ------------
-;  Apartado 2
+;  Section 2
 ; ------------
 MOV M, A
 ; ------------
-;  Apartado 3
+;  Section 3
 ; ------------
 INX H
 MOV M, B
@@ -30,26 +31,26 @@ MOV M, D
 INX H
 MOV M, E
 ; ------------
-;  Apartado 4
+;  Section 4
 ; ------------
 SHLD 60FH
 ; ------------
-;  Apartado 5
+;  Section 5
 ; ------------
 XCHG
 MOV M, A
 ; ------------
-;  Apartado 6
+;  Section 6
 ; ------------
 LHLD 60FH
 MOV M, C
 ; ------------
-;  Apartado 7
+;  Section 7
 ; ------------
 STAX B
 MVI A, 44H
 STAX D
 ; -----
-;  FIN
+;  END
 ; -----
 HLT 
