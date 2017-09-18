210d46914c8d7c4f265e3a38c58723d90715ce5d
Roderick Diaz <irodri@hotmail.es>
2017-09-18 13:40:21 +0200

    Update P7.asm

--- P7.asm
+++ P7.asm
@@ -1,200 +1,202 @@
 ; ---------------------------------
-;  AUTOR: Díaz García, Rodrigo
-;  FECHA: 12 Abril 2017
-;  PRUEBAS REALIZADAS: Práctica 7
+;  AUTHOR: Díaz García, Rodrigo
+;  DATE: 12 April 2017
+;  ASSIGNMENT: P7
 ; ---------------------------------
 
-OUTPT EQU 041DH ; Establece posición de la función OUTPT
-RDKBD EQU 044EH ; Establece posición de la función RDKBD
+OUTPT EQU 041DH ; Function Output
+RDKBD EQU 044EH ; Function Read Keyboard
 
-ORG 1000H ; Comienzo de datos
-MVI A, 08H ; Inicializa A, a mascara de interrupciones
-MVI B, FFH ; Inicializa B
-MVI C, FFH ; Inicializa C
-LXI SP, 2000H ; Inicializa SP
-SIM ; Establece la máscara
-EI ; Habilita interrupciones
-REP: ; Ciclo principal
-	CALL RDKBD ; Espera y devuelve una tecla pulsada en A
-	CALL CHKKP ; Comprueba que hacer con la tecla
-	JMP REP ; Repite ciclo
+ORG 1000H
+MVI A, 08H ; Initialize A as interrupt mask
+MVI B, FFH ; Initialize B
+MVI C, FFH ; Initialize C
+LXI SP, 2000H ; Initialize SP
+SIM ; Set iterrupt mask
+EI ; Enable interrupt
+REP: ; Main loop
+	CALL RDKBD ; Wait until keypress
+	CALL CHKKP ; Check key
+	JMP REP ; Repeat
 
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-; 	@function		CHKKP
-; 	@abstract		Comprueba la telca pulsada
-;	@discussion		Interpreta la tecla pulsada en
-;					el acumulador, si es un número, lo
-;					guarda, si es un operando, realiza
-;					la operación.
-;	@input	A		ASCII de tecla pulsada	
-;	@result M1600	Primer código introducido
-;	@result M1601	Segundo código introducido
-;	@result M1602	Código del primer dígito del resultado
-;	@result M1603	Código del segundo dígito del resultado
-;	@result M1604	Modo de operacion (0 = -, 1 = +)
-;	@result M1605	Signo resultante (0 = -, 1 = +)
+; 	@function	CHKKP
+; 	@abstract	Checks the pressed key
+;	@discussion	Checks pressed key in port 0,
+;			stores numbers or executes
+;			mathematical operation.
+;	@input	A	Pressed key's ASCII
+;	@result M1600	First number's ASCII
+;	@result M1601	Second number's ASCII
+;	@result M1602	First result number's ASCII
+;	@result M1603	Second result number's ASCII
+;	@result M1604	Operation mode (0 = -, 1 = +)
+;	@result M1605	Result sign (0 = -, 1 = +)
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 CHKKP:
-	CPI 41H ; Comprobar si es a
-	JZ KPADD ; Si lo es, realizar suma
-	CPI 61H ; Comprobar si es A
-	JZ KPADD ; Si lo es, realizar suma
-	CPI 46H ; Comprobar si es f
-	JZ KPSUB ; Si lo es, realizar resta
-	CPI 66H ; Comprobar si es F
-	JZ KPSUB ; Si lo es, realizar resta
-	CPI 3AH ; Comprobar si es mayor de 39
-	RNC ; Si lo es, finalizar función
-	CPI 30H ; Comprobar si es mayor de 30
-	JNC KPNUM ; Si lo es, guardar numero
-	RET ; Finalizar función
+	CPI 41H ; is key = 'a'?
+	JZ KPADD ; if true, addition
+	CPI 61H ; is key = 'A'?
+	JZ KPADD ; if true, addition
+	CPI 46H ; is key = 'f'?
+	JZ KPSUB ; if true, subtraction
+	CPI 66H ; is key = 'F'?
+	JZ KPSUB ; if true, subtraction
+	CPI 3AH ; is key > 39?
+	RNC ; if true, return
+	CPI 30H ; is key > 30?
+	JNC KPNUM ; if true, store number
+	RET ; Return
 
 
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-; 	@subroutine		KPADD
-; 	@abstract		Suma dos números
-;	@discussion		Suma dos números de una cifra,
-;					convierte el resultado en decimal y
-;					lo guarda en memoria
-;	@input	B		Primer operando
-;	@input	C		Segundo operando
-;	@result M1602	Código del primer dígito del resultado
-;	@result M1603	Código del segundo dígito del resultado
-;	@result M1604	1 (Suma)
-;	@result M1605	1 (Positivo)
+; 	@subroutine	KPADD
+; 	@abstract	Adds two numbers
+;	@discussion	Adds two single digit numbers,
+;			stores result in memory.
+;	@input	B	First operand
+;	@input	C	Second operand
+;	@result M1602	First result number's ASCII
+;	@result M1603	Second result number's ASCII
+;	@result M1604	1 (Addition)
+;	@result M1605	1 (Positive)
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 KPADD:
-	LXI H, 1602H ; Comienzo de datos resultantes
+	LXI H, 1602H
 	MOV A, B ; A = B
 	ADD C ; A = B + C 
-	DAA ; Convertir A a BCD
-	MOV E, A ; Guardar resultado en E
-	ANI F0H ; Obtener primer digito
+	DAA ; Convert A to BCD
+	MOV E, A ; Store result in E
+	ANI F0H ; Get first digit
 	RAR ; A=A/10
 	RAR ; A=A/10
 	RAR ; A=A/10
 	RAR ; A=A/10
-	ADI 30H ; Obtener código ASCII
-	MOV M, A ; Guardar código del primer dígito
-	INX H ; Siguiente posición de memoria
-	MOV A, E ; Cargar resultado
-	ANI FH ; Obtener segundo digito
-	ADI 30H ; Obtener código ASCII
-	MOV M, A ; Guardar código del segundo dígito
-	INX H ; Siguiente posición de memoria
-	MVI M, 1H ; Establecer modo a suma
-	INX H ; Siguiente posición de memoria
-	MVI M, 1H ; Establecer signo a positivo
-	CALL UPDATE ; Llamar función escribir en pentalla
-	MVI B, FFH ; Vaciar B
-	MVI C, FFH ; Vaciar C
-	RET ; Finalizar función CHKKP
+	ADI 30H ; Obtain ASCII
+	MOV M, A ; Store first result number's ASCII
+	INX H ; Next memory position
+	MOV A, E ; Load result
+	ANI FH ; Get second digit
+	ADI 30H ; Obtain ASCII
+	MOV M, A ; Store second result number's ASCII
+	INX H
+	MVI M, 1H ; mode = addition
+	INX H
+	MVI M, 1H ; sign = positive
+	CALL UPDATE ; Update screen
+	MVI B, FFH ; Empty B
+	MVI C, FFH ; Empty C
+	RET ; Return
 	
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-; 	@subroutine		KPSUB
-; 	@abstract		Resta dos números
-;	@discussion		Resta dos números de una cifra en,
-;					obtiene el valor absoluto y guarda el
-;					signo de la resta.
-;	@input	B		Primer operando
-;	@input	C		Segundo operando
+; 	@subroutine	KPSUB
+; 	@abstract	Subtracts two numbers
+;	@discussion	Subtracts two single digit numbers,
+;			obtains absolute value, and stores
+;			result and sign in memory.
+;	@input	B	First operand
+;	@input	C	Second operand
 ;	@result M1602	30 (0)
-;	@result M1603	Código del resultado absoluto (|A-B|)
-;	@result M1604	0 (Resta)
-;	@result M1605	Signo resultante (0 = -, 1 = +)
+;	@result M1603	Absolute result's ASCII (|A-B|)
+;	@result M1604	0 (Subtraction)
+;	@result M1605	Result sign (0 = -, 1 = +)
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 KPSUB:
-	LXI H, 1602H ; Comienzo de datos resultantes
-	MVI M, 30H ; Guardar primer código como 0
-	INX H ; Siguiente posición de memoria
+	LXI H, 1602H
+	MVI M, 30H ; |Result| will always be < 10
+	INX H
 	MOV A, B ; A = B
-	CMP C ; Comprobar que operando es mayor
-	JC ABS ; Si el segundo es mayor, ve al método ABS
+	CMP C ; is C > B?
+	JC ABS ; if true go to ABS
 		SUB C ; A = B - C 
-		MOV E, A ; Guardar resultado en E
-		ADI 30H ; Obtener código ASCII
-		MOV M, A ; Guardar código del resultado
-		INX H ; Siguiente posición de memoria
-		MVI M, 0H ; Establecer modo a resta
-		INX H ; Siguiente posición de memoria
-		MVI M, 1H ; Establecer signo a positivo
-		CALL UPDATE ; Llamar función escribir en pentalla
-		MVI B, FFH ; Vaciar B
-		MVI C, FFH ; Vaciar C
-		RET ; Finalizar función CHKKP
+		MOV E, A ; Store result in E
+		ADI 30H ; Obtain ASCII
+		MOV M, A ; Store result's ASCII
+		INX H
+		MVI M, 0H ; mode = subtraction
+		INX H
+		MVI M, 1H ; sign = positive
+		CALL UPDATE ; Update screen
+		MVI B, FFH ; Empty B
+		MVI C, FFH ; Empty C
+		RET ; Return
 	
-	ABS: ; Método ABS, calcula la resta y el absoluto
+	ABS:
 		MOV A, B ; A = B
 		SUB C ; A = B - C 
 		XRI FFH ; A = NOT A
-		ADI 1H ; Corregir desviación de XOR
-		MOV E, A ; Guardar resultado en E
-		ADI 30H ; Obtener código ASCII
-		MOV M, A ; Guardar código del resultado
-		INX H ; Siguiente posición de memoria
-		MVI M, 0H ; Establecer modo a resta
-		INX H ; Siguiente posición de memoria
-		MVI M, 0H ; Establecer signo a negativo
-		CALL UPDATE ; Llamar función escribir en pentalla
-		MVI B, FFH ; Vaciar B
-		MVI C, FFH ; Vaciar C
-		RET ; Finalizar función CHKKP
+		ADI 1H ; Fix XOR deviation
+		MOV E, A ; Store result in E
+		ADI 30H ; Obtain ASCII
+		MOV M, A ; Store result's ASCII
+		INX H
+		MVI M, 0H ; mode = subtraction
+		INX H
+		MVI M, 0H ; sign = negative
+		CALL UPDATE ; Update screen
+		MVI B, FFH ; Empty B
+		MVI C, FFH ; Empty C
+		RET ; Return
 
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-; 	@subroutine		KPNUM
-; 	@abstract		Almacena número
-;	@discussion		Obtiene el número a partir del código
-;					y lo guarda en B/M si este esta vacio
-;					o en C/M.
-;	@input	A		Código introducido
-;	@result M1600	Primer código introducido
-;	@result M1601	Segundo código introducido
+; 	@subroutine	KPNUM
+; 	@abstract	Stores number
+;	@discussion	Decides where to store number,
+;			B/1600 for the first one and
+;			C/1601 for the second one.
+;	@input	A	Number's ASCII
+;	@result M1600	First number's ASCII
+;	@result B	First number
+;	@result M1601	Second number's ASCII
+;	@result C	Second number
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 KPNUM:
-	PUSH PSW ; Guardar AF
-	MOV A, B ; Obtener primer numero guardado
-	CPI FFH ; Comprobar que este vacio
-	JNZ ELSE ; Si no lo esta, ir a else
-		POP PSW ; Cargar AF guardado
-		STA 1600H ; Guardar como primer código
-		SUI 30H ; Restar 30 para obtener el numero
-		MOV B, A ; Guardar numero en B
-		RET ; Finalizar interrupción
+	PUSH PSW ; Store state
+	MOV A, B ; obtain first stored number
+	CPI FFH ; is it empty?
+	JNZ ELSE ; if false, go to else
+		POP PSW ; Load state
+		STA 1600H ; Store as first number
+		SUI 30H ; Obtain number
+		MOV B, A ; Store number in B
+		RET ; Return
 
 	ELSE:
-		POP PSW ; Cargar AF guardado
-		STA 1601H ; Guardar como segundo código
-		SUI 30H ; Restar 30 para obtener el numero
-		MOV C, A ; Guardar numero en C
-		RET ; Finalizar función CHKKP
+		POP PSW ; Load state
+		STA 1601H ; Store as second number
+		SUI 30H ; Obtain number
+		MOV C, A ; Store number in C
+		RET ; Return
 
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-; 	@function		UPDATE
-; 	@abstract		Escribe operación en pantalla
-;	@discussion		Escribe los 2 numeros introducidos,
-;					el modo de operación, el signo, y
-;					el resultado.
-;	@input M1600	Primer código introducido
-;	@input M1601	Segundo código introducido
-;	@input M1602	Primer código del resultado
-;	@input M1603	Segundo código del resultado
-;	@input M1604	Modo de operacion (0 = -, 1 = +)
-;	@input M1605	Signo resultante (0 = -, 1 = +)
+; 	@function	UPDATE
+; 	@abstract	Shows operation in screen
+;	@discussion	Escribe los 2 numeros introducidos,
+;			el modo de operación, el signo, y
+;			el resultado.
+;	@discussion	Writes the 2 operands, the mode,
+;			the sign, and the result.
+;	@input M1600	First operand's ASCII
+;	@input M1601	Second operand's ASCII
+;	@input M1602	First result digit's ASCII
+;	@input M1603	Second result digit's ASCII
+;	@input M1604	Operation mode (0 = -, 1 = +)
+;	@input M1605	Result sign (0 = -, 1 = +)
 ; * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 UPDATE:
-	LXI H, 1604H ; Obtener operación
-	MVI A, 0H ; Seleccionar campo de dirección
-	MOV B, M ; Escribir punto si es una resta
-	LXI H, 1600H ; Escribir dos primeros números
-	OUTPT ; Escribir en pantalla
-	LXI H, 1605H ; Obtener signo
-	MVI A, 1H; Seleccionar campo de datos
-	MOV B, M ; Escribir punto si es negativo
-	LXI H, 1602H ; Escribir resultado
-	OUTPT ; Escribir en pantalla
-	RET ; Finalizar función
+	LXI H, 1604H ; Get mode
+	MVI A, 0H ; Select first screen half
+	MOV B, M ; Write dash if mode is subtraction
+	LXI H, 1600H ; Write both operands
+	OUTPT ; Write screen
+	LXI H, 1605H ; Get sign
+	MVI A, 1H; Select second screen half
+	MOV B, M ; Write dot if sign is negative
+	LXI H, 1602H ; Write result
+	OUTPT ; Write screen
+	RET ; Return
